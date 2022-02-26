f4048c91e8f907df8e38fc8062106abf3d83a35d
skyoo1226 <you1226@naver.com>
2022-02-26 21:51:55 +0900

    OtherActivity 화면으로 이동하는 코드입력

--- app/src/main/java/com/skyoo/intent_20220226_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/intent_20220226_exam/MainActivity.kt
@@ -2,10 +2,23 @@ package com.skyoo.intent_20220226_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+// 유투브 49:00참조
+
+        btnMoveOther.setOnClickListener {
+
+            // OtherActivity 화면으로 이동하는 코드...
+
+
+        }
+
     }
+
+
 }

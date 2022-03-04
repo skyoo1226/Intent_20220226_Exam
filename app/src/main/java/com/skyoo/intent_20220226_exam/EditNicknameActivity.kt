3f51d73d865bac7e00e89cb91f0bf3c795a32520
skyoo1226 <you1226@naver.com>
2022-03-04 10:53:06 +0900

    닉네임을 받기 위한 화면 이동 시 startActivityForResult 옛날 코드를 활용해 봄. - 닉네임 변경화면의 저장버튼에 새로 입력한 닉네임을 putExtra로 메인으로 복귀만 하도록 함. 아직 메인에 새로운 닉네임 적용 안됨.

--- app/src/main/java/com/skyoo/intent_20220226_exam/EditNicknameActivity.kt
+++ app/src/main/java/com/skyoo/intent_20220226_exam/EditNicknameActivity.kt
@@ -1,11 +1,34 @@
 package com.skyoo.intent_20220226_exam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnSave.setOnClickListener {
+
+//            입력한 닉네임을 변수로 담고,
+            val inputNickname = edtNickname.text.toString()
+
+//            putExtra를 통해서 데이터를 담아주는 용도의 Intent 생성.
+            // 출발지와 도착지 명시 필요 X
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+
+//            OK누른게 맞는지? + 어떤 데이터를 가지고 돌아갈건지 => 결과로 설정.
+//            백 버튼을 누르면 변경 없이 그냥 돌아 감.
+            setResult(RESULT_OK, resultIntent)
+
+//            이전 화면으로 복귀
+            finish()
+
+
+        }
+
     }
 }

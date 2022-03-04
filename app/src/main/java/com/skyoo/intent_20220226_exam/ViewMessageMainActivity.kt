0d588d6ce8e929468c163f0ce446fa3aeb44c659
skyoo1226 <you1226@naver.com>
2022-03-04 10:01:27 +0900

    메인화면에 데이터+수정버튼 만들고, 데이터과 버튼 사이에 TextView로 오른쪽으로 밀어 내는 코드를 넣어 줌.

--- app/src/main/java/com/skyoo/intent_20220226_exam/ViewMessageMainActivity.kt
+++ app/src/main/java/com/skyoo/intent_20220226_exam/ViewMessageMainActivity.kt
@@ -2,10 +2,18 @@ package com.skyoo.intent_20220226_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message_main.*
 
 class ViewMessageMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message_main)
+
+//        이전화면에서 보내준 메세지(문구)를 변수에 담아 둠
+        val message = intent.getStringExtra("메세지")
+
+//         위 변수에 담아 둔 메세지(문구)를 텍스트뷰의 text속성에 set 함.
+        txtMessage.text = message
+
     }
 }

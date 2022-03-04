1673ba81ad442fa20ec5f001fa5685b2cb847afe
skyoo1226 <you1226@naver.com>
2022-03-04 09:18:37 +0900

    이전 화면 종료해야지만 메인화면으로 돌아옴 - 메인화면에 메세지를 입력 후 다른 화면으로 보내기 - send메세지가 눌리면 보낸 메세지(edtMesage)를 ViewMessageActivity로 메세지를 가지고 화면 이동

--- app/src/main/java/com/skyoo/intent_20220226_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/intent_20220226_exam/MainActivity.kt
@@ -18,6 +18,14 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, OtherActivity::class.java)
             startActivity(myIntent)
 
+        }
+
+        btnSendMessage.setOnClickListener {
+
+//            send메세지가 눌리면 보낸 메세지(edtMesage)를 보여줘야하는데,
+        //            이때 ViewMessageActivity로 메세지를 가지고 화면 이동
+            val  myIntent = Intent(this, ViewMessageMainActivity::class.java)
+            startActivity(myIntent)
 
         }
 
